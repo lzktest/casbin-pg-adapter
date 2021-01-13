7d34524ee32b906a42f6e37b8c58af35e512c4fa
unknown <aaa@example.com>
2021-01-13 09:55:34 +0800

    upload md5.go

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
-module casbinpgandadapter
+module github.com/lzktest/casbinpgandadapter
 
 go 1.15
+
+require (
+	github.com/casbin/casbin/v2 v2.20.0
+	github.com/lib/pq v1.9.0
+	github.com/stretchr/testify v1.6.1
+)
---
+++ hash/md5_test.go
@@ -0,0 +1,35 @@
+package hash
+
+import "testing"
+
+func TestMd5(t *testing.T) {
+	const expectFileMd5 = "cf1cafaeb10fc123e1b68fa8613c4a86"
+	actualFileMd5, err := FileMd5("../adapter.go")
+	if err != nil {
+		panic(err)
+	}
+
+	if expectFileMd5 != actualFileMd5 {
+		t.Errorf("expect file md5 is %s; but had %s\n", expectFileMd5, actualFileMd5)
+	}
+
+	const str = "why did you like golang"
+	const expectStringMd5 = "09a6f16fc1e802003b4c0c11b69761d2"
+	actualStringMd5 := StringMd5(str)
+	if expectStringMd5 != actualStringMd5 {
+		t.Errorf("expect string md5 value is %s; but had %s\n", expectStringMd5, actualStringMd5)
+	}
+}
+
+func BenchmarkMd5(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_, err := FileMd5("./md5.go")
+		if err != nil {
+			panic(err)
+		}
+	}
+	const str = "why did you like golang"
+	for i := 0; i < b.N; i++ {
+		_ = StringMd5(str)
+	}
+}
